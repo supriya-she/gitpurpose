5424e3aed14277da4b175d3c08bd3c0d78aaf860
Supriya she <Supriyashelke838@gmail.com git config --global user.email Supriyashelke838@gmail.com>
2024-12-24 00:16:00 +0530

    first commit

---
+++ src/app/labtest/labtest.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labtest',
+  imports: [],
+  templateUrl: './labtest.component.html',
+  styleUrl: './labtest.component.css'
+})
+export class LabtestComponent {
+
+}
